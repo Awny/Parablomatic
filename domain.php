dc75baf10ebd0956faadd90633b41b5dbc8b9ea5
buildingnanalyzing <eduardoftwlandin@gmail.com>
2016-03-13 18:03:55 +0300

    Create domain.php

---
+++ domain.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <link rel="stylesheet" href="parabolamatic.css">
+  <title>Parablomatic</title>
+ </head>
+ <body>
+  <div id="header">
+   <a href="parabolamatic.html">Parablomatic</a>
+  </div>
+  <div id="content">
+   <p class="title">The domain and range of quadratic functions</p>
+   <p>Normal quadratic functions without any restrictions on the x axis will always have
+   a domain of "x such that x is an element of the real numbers", because unlike rational, 
+   logarithmic or square root functions, there are no x values in a quadratic that could 
+   provide an undefined or "impossible" value.</p>
+   <p>On the other hand, because the vertex in a quadratic represents the highest/lowest
+   y value that the quadratic will ever have, the range of the quadratic will always be
+   "y such that y is greater/less than or equal to *y coordinate of the vertex*". Whether
+   the range includes a "greater than or equal to" or a "less than or equal to" depends
+   whether the quadratic opens upwards or downwards (if the "a" value on the equation is
+   positive or negative). In those cases where the parabola is going down, the range will
+   be "less than or equal to" as in those cases the vertex represents the MAXIMUM value
+   of the parabola. Meanwhile, in the cases where the parabola is going up, the range
+   will be "greater than or equal to" as in those cases the vertex represents the MINIMUM
+   value of the parabola.</p>
+   <p class="title">Let's practice!</p>
+   <p>f(x)=5(x+5)<sup>2</sup>-3</p>
+   <p>Find the domain and range:</p>
+   <form method="post" action="domain.php">
+    <input type="text" name="domain" id="domain" placeholder="x such that..."> <br>
+    <input type="text" name="range" id="range" placeholder="y such that..."> <br>
+    <input type="submit">
+   </form>
+   <?php
+    $domain = $_POST['domain'];
+    $range = $_POST['range'];
+    
+    if($domain == "x such that x is an element of the real numbers") {
+        echo "Correct domain! Well done!";
+    } else {
+        echo "Incorrect! Try again!";
+    }
+    if($range == "y such that y is greater than or equal to negative three") {
+        echo "Correct range! Well done!";
+    } else { 
+        echo "Incorrect! Try again!";
+    }          
+   ?>
+  </div>   
+ </body>
+</html>
