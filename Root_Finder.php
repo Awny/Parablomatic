42e14cf2554e2dceb4ef7c25a0941af6e5f3e2d2
Awny <mhd.awny@yahoo.com>
2016-03-15 20:22:31 +0300

    fixed output error where it is on same line as bottom links

--- Root_Finder.php
+++ Root_Finder.php
@@ -44,6 +44,7 @@ echo substr($x1,0,5);
 echo substr($x2,0,6);
         
         ?>
+        <br />
         <a href="standard_from.html">Standard form</a>
         <a href="vertex_form.html">Vertex and vertex form</a>
         <a href="Factored_form.html">X intercepts and factored form</a>
